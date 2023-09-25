3530c0065e44dd4821f9d2ccd18844c7b8c18c5d
homo-errantium <homo.errantium@gmail.com>
2023-09-25 21:21:21 +0300

    changed validationUser.js (added pattern(EMAIL_REGEX) to email pull

--- middlewares/validation/validationUser.js
+++ middlewares/validation/validationUser.js
@@ -1,16 +1,18 @@
 const { celebrate, Joi } = require('celebrate');
 
+const EMAIL_REGEX = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-].[a-zA-Z0-9-.]+$/;
+
 module.exports.validationCreateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    email: Joi.string().required().email(),
+    email: Joi.string().required().pattern(EMAIL_REGEX),
     password: Joi.string().required().min(8),
   }),
 });
 
 module.exports.validationLogin = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().required().pattern(EMAIL_REGEX),
     password: Joi.string().required().min(8),
   }),
 });
@@ -18,12 +20,6 @@ module.exports.validationLogin = celebrate({
 module.exports.validationUpdateUserInfo = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    email: Joi.string().required().email(),
+    email: Joi.string().required().pattern(EMAIL_REGEX),
   }),
 });
-
-// module.exports.validationUserID = celebrate({
-//   params: Joi.object().keys({
-//     userId: Joi.string().hex().length(24),
-//   }),
-// });
